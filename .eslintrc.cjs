1b4fba7791274938fde2c6343f4769db31e0ce83
Zena <33304871+Heojiyeon@users.noreply.github.com>
2024-01-02 09:51:16 +0900

    chore: 프로젝트 초기 세팅

---
+++ .eslintrc.cjs
@@ -0,0 +1,64 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint',
+    'prettier',
+    'eslint-plugin-simple-import-sort',
+    'unused-imports',
+    'import',
+  ],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-empty-function': 'off',
+    'react-hooks/exhaustive-deps': ['warn'],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['builtin', 'external'],
+          'internal',
+          ['parent', 'sibling'],
+          'index',
+        ],
+        'newlines-between': 'always',
+        pathGroups: [
+          {
+            pattern: '@/**',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+      },
+    ],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports-ts': ['error'],
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import AppRouter from './Router';
+
+const App = () => {
+  return (
+    <>
+      <AppRouter />
+    </>
+  );
+};
+
+export default App;
