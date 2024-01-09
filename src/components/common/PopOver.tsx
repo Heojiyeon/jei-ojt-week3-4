344a6dbde5f03a7b8f0c2c5726a0b40ab5781ade
Zena <33304871+Heojiyeon@users.noreply.github.com>
2024-01-09 15:21:36 +0900

    feat: 공통 컴포넌트(Popover) 컴포넌트 구현

---
+++ src/components/common/PopOver.tsx
@@ -0,0 +1,37 @@
+import {
+  Button,
+  Popover,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
+  Portal,
+} from '@chakra-ui/react';
+import { ReactNode } from 'react';
+
+type PopOverProp = {
+  trigger: ReactNode;
+  content: ReactNode;
+};
+
+const PopOver = ({ trigger, content }: PopOverProp) => {
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Button style={{ border: 'none' }}>{trigger}</Button>
+      </PopoverTrigger>
+      <Portal>
+        <PopoverContent
+          style={{
+            backgroundColor: '#ffffff',
+            width: 200,
+            height: 70,
+          }}
+        >
+          <PopoverBody>{content}</PopoverBody>
+        </PopoverContent>
+      </Portal>
+    </Popover>
+  );
+};
+
+export default PopOver;
