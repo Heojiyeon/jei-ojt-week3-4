b472ad91bac35f44d402d96b10b43e640fb63a13
Zena <33304871+Heojiyeon@users.noreply.github.com>
2024-01-11 09:44:17 +0900

    refactor: handleComponent 유틸 함수 분리 및 적용

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -18,6 +18,14 @@ import Pagination from '@/components/Pagination';
 import Toolbar from '@/components/Toolbar';
 import View from '@/components/View';
 import Modal from '@/components/common/Modal';
+import {
+  addExistedCircleComponent,
+  addExistedGroupComponent,
+  addExistedImageComponent,
+  addExistedLineComponent,
+  addExistedRectComponent,
+  addExistedTextComponent,
+} from '@/utils/handleComponent';
 import { fabric } from 'fabric';
 import { Ellipse, Polyline, Rect, Textbox } from 'fabric/fabric-impl';
 import { useAtom, useAtomValue, useSetAtom } from 'jotai';
@@ -42,239 +50,6 @@ const MainPage = () => {
     setIsOpenModal(prevIsOpenModal => !prevIsOpenModal);
   };
 
-  /**
-   * 캔버스 내 line 컴포넌트 추가 함수
-   */
-  const addLineComponent = (data: Polyline) => {
-    const newLine = new fabric.Polyline(data?.points!, {
-      width: data.width,
-      height: data.height,
-      stroke: data.stroke,
-      top: data.top,
-      left: data.left,
-      type: 'line',
-    });
-
-    // 추가적인 속성에 따른 생성
-    if (data.angle) {
-      newLine.set('angle', data.angle);
-    }
-    if (data.scaleX) {
-      newLine.set('scaleX', data.scaleX);
-      newLine.set('scaleY', data.scaleY);
-    }
-    if (data.strokeWidth) {
-      newLine.set('strokeWidth', data.strokeWidth);
-    }
-    if (data.strokeDashArray) {
-      newLine.set('strokeDashArray', data.strokeDashArray);
-    }
-
-    if (canvasRef.current !== null) {
-      canvasRef.current.add(newLine);
-      canvasRef.current.requestRenderAll();
-    }
-    return newLine;
-  };
-
-  /**
-   * 캔버스 내 Circle 컴포넌트 추가 함수
-   */
-  const addCircleComponent = (data: Ellipse) => {
-    const newCircle = new fabric.Ellipse({
-      selectable: false,
-      width: data.width,
-      height: data.height,
-      fill: data.fill,
-      stroke: data.stroke,
-      top: data.top,
-      left: data.left,
-      rx: data.rx,
-      ry: data.ry,
-      type: 'circle',
-    });
-
-    // 추가적인 속성에 따른 생성
-    if (data.angle) {
-      newCircle.set('angle', data.angle);
-    }
-    if (data.scaleX) {
-      newCircle.set('scaleX', data.scaleX);
-      newCircle.set('scaleY', data.scaleY);
-    }
-    if (data.strokeWidth) {
-      newCircle.set('strokeWidth', data.strokeWidth);
-    }
-    if (data.strokeDashArray) {
-      newCircle.set('strokeDashArray', data.strokeDashArray);
-    }
-
-    if (canvasRef.current !== null) {
-      canvasRef.current.add(newCircle);
-      canvasRef.current.requestRenderAll();
-    }
-    return newCircle;
-  };
-
-  /**
-   * 캔버스 내 Rect 컴포넌트 추가 함수
-   */
-  const addRectComponent = (data: Rect) => {
-    const newRect = new fabric.Rect({
-      selectable: false,
-      width: data.width,
-      height: data.height,
-      fill: data.fill,
-      stroke: data.stroke,
-      top: data.top,
-      left: data.left,
-      type: 'rect',
-    });
-    // 추가적인 속성에 따른 생성
-    if (data.angle) {
-      newRect.set('angle', data.angle);
-    }
-    if (data.scaleX) {
-      newRect.set('scaleX', data.scaleX);
-      newRect.set('scaleY', data.scaleY);
-    }
-    if (data.strokeWidth) {
-      newRect.set('strokeWidth', data.strokeWidth);
-    }
-    if (data.strokeDashArray) {
-      newRect.set('strokeDashArray', data.strokeDashArray);
-    }
-
-    if (canvasRef.current !== null) {
-      canvasRef.current.add(newRect);
-      canvasRef.current.requestRenderAll();
-    }
-    return newRect;
-  };
-
-  /**
-   * 캔버스 내 텍스트 컴포넌트 추가 함수
-   */
-  const addTextComponent = (data: Textbox) => {
-    const newText = new fabric.Textbox('Text', {
-      stroke: data.stroke,
-      minWidth: data.minWidth,
-      width: data.width,
-      height: data.height,
-      top: data.top,
-      left: data.left,
-      splitByGrapheme: data.splitByGrapheme,
-      styles: data.styles,
-      text: data.text,
-      type: 'text',
-    });
-    // 추가적인 속성에 따른 생성
-    if (data.angle) {
-      newText.set('angle', data.angle);
-    }
-    if (data.scaleX) {
-      newText.set('scaleX', data.scaleX);
-      newText.set('scaleY', data.scaleY);
-    }
-    if (data.strokeWidth) {
-      newText.set('strokeWidth', data.strokeWidth);
-    }
-    if (data.strokeDashArray) {
-      newText.set('strokeDashArray', data.strokeDashArray);
-    }
-
-    if (canvasRef.current !== null) {
-      canvasRef.current.add(newText);
-      canvasRef.current.requestRenderAll();
-    }
-    return newText;
-  };
-
-  /**
-   * 캔버스 내 이미지 컴포넌트 추가 함수
-   */
-  const addImageComponent = (data: addedImage) => {
-    const newImage = fabric.Image.fromURL(
-      data!.src,
-      function (img) {
-        img.set('top', data.top);
-        img.set('left', data.left);
-
-        // 추가적인 속성에 따른 생성
-        if (data.angle) {
-          img.set('angle', data.angle);
-        }
-        if (data.scaleX) {
-          img.set('scaleX', data.scaleX);
-          img.set('scaleY', data.scaleY);
-        }
-        if (data.strokeWidth) {
-          img.set('strokeWidth', data.strokeWidth);
-        }
-        if (data.strokeDashArray) {
-          img.set('strokeDashArray', data.strokeDashArray);
-        }
-        if (canvasRef.current !== null) {
-          canvasRef.current.add(img);
-          canvasRef.current.requestRenderAll();
-        }
-
-        return img;
-      },
-      {
-        crossOrigin: 'Anonymous',
-      }
-    );
-    return newImage;
-  };
-
-  const addGroupComponent = (data: addedGroup) => {
-    // objects에 따라 생성
-    const createdObjects: TargetComponent[] = [];
-
-    data.objects.map(object => {
-      switch (object.type) {
-        case 'image':
-          const newImage = addImageComponent(object as addedImage);
-          createdObjects.push(newImage);
-          break;
-
-        case 'text':
-          const newText = addTextComponent(object as Textbox);
-          createdObjects.push(newText);
-          break;
-
-        case 'rect':
-          const newRect = addRectComponent(object as Rect);
-          createdObjects.push(newRect);
-          break;
-
-        case 'circle':
-          const newCircle = addCircleComponent(object as Ellipse);
-          createdObjects.push(newCircle);
-          break;
-
-        case 'line':
-          const newLine = addLineComponent(object as Polyline);
-          createdObjects.push(newLine);
-          break;
-
-        default:
-          break;
-      }
-    });
-    // group에 추가
-    const newGroup = new fabric.Group(createdObjects, {
-      top: data.top,
-      left: data.left,
-    });
-
-    if (canvasRef.current !== null) {
-      canvasRef.current.add(newGroup);
-      canvasRef.current.renderAll();
-    }
-  };
-
   useEffect(() => {
     if (isOpenPreviewModal) {
       canvasRef.current = new fabric.Canvas('preview-canvas', {
@@ -290,38 +65,62 @@ const MainPage = () => {
         window.localStorage.getItem('entireComponent');
 
       if (entireComponentData) {
-        JSON.parse(entireComponentData).map((component: TargetComponent) => {
-          // console.log(typeof component, component as Group);
-          switch (component.type) {
-            case 'group':
-              addGroupComponent(component as addedGroup);
-              break;
+        JSON.parse(entireComponentData).map(
+          async (component: TargetComponent) => {
+            // console.log(typeof component, component as Group);
+            switch (component.type) {
+              case 'group':
+                const createdGroup = addExistedGroupComponent(
+                  component as addedGroup
+                );
+                canvasRef.current?.add(createdGroup as addedGroup);
+                canvasRef.current?.requestRenderAll();
+                break;
 
-            case 'image':
-              addImageComponent(component as addedImage);
-              break;
+              case 'image':
+                const createdImage = await addExistedImageComponent(
+                  component as addedImage
+                );
+                canvasRef.current?.add(createdImage as addedImage);
+                canvasRef.current?.requestRenderAll();
+                break;
 
-            case 'text':
-              addTextComponent(component as Textbox);
-              break;
+              case 'text':
+                const createdText = addExistedTextComponent(
+                  component as Textbox
+                );
+                canvasRef.current?.add(createdText as Textbox);
+                canvasRef.current?.requestRenderAll();
+                break;
 
-            case 'rect':
-              addRectComponent(component as Rect);
-              break;
+              case 'rect':
+                const createdRect = addExistedRectComponent(component as Rect);
+                canvasRef.current?.add(createdRect as Rect);
+                canvasRef.current?.requestRenderAll();
+                break;
 
-            case 'circle':
-              addCircleComponent(component as Ellipse);
-              break;
+              case 'circle':
+                const createdCircle = addExistedCircleComponent(
+                  component as Ellipse
+                );
+                canvasRef.current?.add(createdCircle as Ellipse);
+                canvasRef.current?.requestRenderAll();
+                break;
 
-            case 'line':
-              addLineComponent(component as Polyline);
-              break;
+              case 'line':
+                const createdLine = addExistedLineComponent(
+                  component as Polyline
+                );
+                canvasRef.current?.add(createdLine as Polyline);
+                canvasRef.current?.requestRenderAll();
+                break;
 
-            default:
-              break;
+              default:
+                break;
+            }
+            canvasRef.current?.renderAll();
           }
-          canvasRef.current?.renderAll();
-        });
+        );
       }
 
       return () => {
